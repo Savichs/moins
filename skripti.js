377b9c036acf1e8f7ea97d1a0ff4695d909a388d
Savichs <122868693+Savichs@users.noreply.github.com>
2023-02-07 10:46:35 +0200

    Create skripti.js

---
+++ skripti.js
@@ -0,0 +1,12 @@
+//alert("Čau")
+console.log("test");
+
+var logs = document.querySelector(".chataZinas");
+var vards =document.querySelector("#vards");
+var zina =document.querySelector("#zina");
+
+
+function sutitZinu(){
+    console.log("Poga Darbojas");
+    console.log("ziņu ir: "+zina.value");
+}
